8c5e5d1bf2db2ceec27e19b70d66435b84c82862
Idris Khan <idris.khan303@gmail.com>
2020-08-03 19:54:50 +0100

    Do the media queries next and get responsive images

---
+++ js/src/index.js
@@ -0,0 +1,22 @@
+/* global $ */
+/* eslint-env jquery */
+/* global waypoint */
+
+const $nav = $(".nav");
+const $link = $(".nav__link");
+const date = new Date();
+
+
+$(function() {
+    $(".js--btn-call").waypoint((direction) => {
+        if(direction === "down") {
+            $nav.addClass("nav--light-bar");
+            $link.addClass("nav__link--dark");
+        } else {
+            $nav.removeClass("nav--light-bar");
+            $link.removeClass("nav__link--dark");
+        }
+    });
+
+    $(".footer__legal").html(`Copyright &copy; ${date.getFullYear()} design of Wimborne and Ferndown Cabby`);
+})
