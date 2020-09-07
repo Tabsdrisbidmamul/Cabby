b92060d12847a36214bce5e634ee7ddcde0dfc15
Idris Khan <idris.khan303@gmail.com>
2020-09-07 15:01:50 +0100

    work

--- js/src/index.js
+++ js/src/index.js
@@ -6,17 +6,18 @@ const $nav = $(".nav");
 const $link = $(".nav__link");
 const date = new Date();
 
+$(function () {
+  $(".js--btn-call").waypoint((direction) => {
+    if (direction === "down") {
+      $nav.addClass("nav--light-bar");
+      $link.addClass("nav__link--dark");
+    } else {
+      $nav.removeClass("nav--light-bar");
+      $link.removeClass("nav__link--dark");
+    }
+  });
 
-$(function() {
-    $(".js--btn-call").waypoint((direction) => {
-        if(direction === "down") {
-            $nav.addClass("nav--light-bar");
-            $link.addClass("nav__link--dark");
-        } else {
-            $nav.removeClass("nav--light-bar");
-            $link.removeClass("nav__link--dark");
-        }
-    });
-
-    $(".footer__legal").html(`Copyright &copy; ${date.getFullYear()} of Wimborne and Ferndown Cabby`);
-})
+  $(".footer__legal").html(
+    `Copyright &copy; 2020-${date.getFullYear()} of Wimborne and Ferndown Cabby`
+  );
+});
